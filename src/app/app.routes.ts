4457d2948f60b4df56c7fe611263387662393256
Kishan Kalavadiya <36093612+kishumohi@users.noreply.github.com>
2025-11-05 17:41:39 +0530

    03A_Basic_Layout_Final_Update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AppLayout } from './components/app-layout/app-layout';
+import { AppDashboard } from './pages/app-dashboard/app-dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AppLayout, children: [{ path: '', component: AppDashboard }] },
+];
--- src/app/components/app-layout/app-layout.ts
+++ src/app/components/app-layout/app-layout.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { AppHeader } from './app-header/app-header';
 import { AppBody } from './app-body/app-body';
 import { AppSidebar } from './app-sidebar/app-sidebar';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-app-layout',
-  imports: [AppHeader, AppBody, AppSidebar],
+  imports: [AppHeader, AppBody, AppSidebar, RouterOutlet],
   templateUrl: './app-layout.html',
   styleUrl: './app-layout.scss',
 })
