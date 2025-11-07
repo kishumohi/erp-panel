e5ff0b0ca25337cf66b1c99a17370ab2094fc549
Kishan Kalavadiya <36093612+kishumohi@users.noreply.github.com>
2025-11-07 18:18:58 +0530

    04C_Table_Create

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import { AppDashboard } from './pages/app-dashboard/app-dashboard';
 import { AppLayout } from './app-layout/app-layout';
+import { AppMaster } from './pages/app-master/app-master';
 
 export const routes: Routes = [
-  { path: '', component: AppLayout, children: [{ path: '', component: AppDashboard }] },
+  {
+    path: '',
+    component: AppLayout,
+    children: [
+      { path: '', component: AppDashboard },
+      { path: 'master', component: AppMaster },
+    ],
+  },
 ];
